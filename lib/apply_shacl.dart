fab4e1c0c030a02d7db7b0376e31404d3ca61431
Isabelle Bryans <bryansi@tcd.ie>
2023-06-03 10:43:39 +0200

    no more logic!

---
+++ lib/apply_shacl.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:simple_chess_board/models/board_arrow.dart';
+import 'package:chess/chess.dart' as chesslib;
+import 'package:simple_chess_board/simple_chess_board.dart';
+
+class apply_shacl {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_chess/my_chess.dart';
 import 'package:simple_chess_board/models/board_arrow.dart';
 import 'package:chess/chess.dart' as chesslib;
 import 'package:simple_chess_board/simple_chess_board.dart';
@@ -6,6 +7,9 @@ import 'package:simple_chess_board/simple_chess_board.dart';
 void main() {
   runApp(const MyApp());
 }
+
+late String prevFen;
+late String currentFen;
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -33,12 +37,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _chess = chesslib.Chess.fromFEN(chesslib.Chess.DEFAULT_POSITION);
+
   var _blackAtBottom = false;
   BoardArrow? _lastMoveArrowCoordinates;
   late ChessBoardColors _boardColors;
 
   @override
   void initState() {
+    currentFen = _chess.fen;
     _boardColors = ChessBoardColors()
       ..lightSquaresColor = Colors.blue.shade200
       ..darkSquaresColor = Colors.blue.shade600
@@ -50,15 +56,32 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
-  void MakeMove({required ShortMove move}) {
+  void saveFen(String fen) {
+    print("saveFen");
+    prevFen = fen;
+  }
+
+  void update_fen() {}
+
+  bool check_shacl({required ShortMove move}) {
+    return true;
+  }
+
+  void FakeMove({required ShortMove move}) {
     print(move.from);
     //print(_chess.board[123]?.type);
+    //update fen
 
     print(chesslib.Chess.SQUARES[move.from]);
     int intfrom = chesslib.Chess.SQUARES[move.from];
     int intto = chesslib.Chess.SQUARES[move.to];
     _chess.board[intto] = _chess.board[intfrom];
     _chess.board[intfrom] = null;
+    //currentFen = _chess.generate_fen();
+    print(currentFen);
+    currentFen = generate_my_fen(_chess.board, _chess.castling, _chess.turn,
+        _chess.ep_square, _chess.half_moves, _chess.move_number);
+    print(currentFen);
     setState(() {});
   }
 
@@ -117,15 +140,27 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: SimpleChessBoard(
           engineThinking: false,
-          fen: _chess.fen,
+          fen: currentFen,
           onMove: ({required ShortMove move}) {
             print('${move.from}|${move.to}|${move.promotion}');
-            MakeMove(move: move);
+            // save previous fen
+            saveFen(currentFen);
+            // make fake move
+            // change fen
+            FakeMove(move: move);
+            // check if shacl allows
+            if (check_shacl(move: move)) {
+              // shacl accepts it
+              // then check real move
+              print("shacl accepts!");
+            }
+            // make floating button to validate move
+            // if pressed: undo fake move, do real move with chesslib
           },
           orientation: BoardColor.white,
           chessBoardColors: _boardColors,
           whitePlayerType: PlayerType.human,
-          blackPlayerType: PlayerType.computer,
+          blackPlayerType: PlayerType.human,
           lastMoveToHighlight: _lastMoveArrowCoordinates,
           onPromote: () => handlePromotion(context),
           onPromotionCommited: ({required ShortMove moveDone}) =>
